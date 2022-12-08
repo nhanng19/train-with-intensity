559312e9c698b0a7a83462d7a8cac8e6b7eec10f
nhan <nhanng19@gmail.com>
2022-12-08 15:42:11 -0800

    added program component

---
+++ src/components/JoinNow/JoinNow.js
@@ -0,0 +1,21 @@
+import "./JoinNow.css";
+const JoinNow = () => {
+  return (
+    <>
+      <section className="banner">
+        <span>join us now</span>
+        <h3>download free programs</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          architecto nesciunt aut sapiente quis inventore quam vitae quod illum
+          incidunt.
+        </p>
+        <a href="#" className="btn">
+          DOWNLOAD
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default JoinNow;
