fa19ddf82b32ef6ca11e3553058d57e516bc6361
nhan <nhanng19@gmail.com>
2022-12-08 15:41:30 -0800

    added contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,22 @@
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <>
+      <section className="banner">
+        <span>join us now</span>
+        <h3>SEND A MESSAGE</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          architecto nesciunt aut sapiente quis inventore quam vitae quod illum
+          incidunt.
+        </p>
+        <a href="#" className="btn">
+          get discount
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default Contact;
